aa588b74e27d1bb7b3eca6ff0a8d1b28ffd1296a
Claus Stilborg <cb@nebulators.com>
2014-10-29 22:01:18 +0100

    Model Picture created and default values defined

---
+++ app/models/Picture.js
@@ -0,0 +1,38 @@
+exports.definition = {
+	config: {
+		columns: {
+		    "title": "text",
+		    "description": "text",
+		    "path": "text",
+		    "location": "text",
+		    "photographer": "text",
+		    "timestamp": "text"
+		},
+		default: {
+			title: "",
+			description: "",
+			path: "",
+			location: "",
+			photographer: "",
+			timestamp: ""
+		},	
+		adapter: {
+			type: "sql",
+			collection_name: "Picture"
+		}
+	},
+	extendModel: function(Model) {
+		_.extend(Model.prototype, {
+			// extended functions and properties go here
+		});
+
+		return Model;
+	},
+	extendCollection: function(Collection) {
+		_.extend(Collection.prototype, {
+			// extended functions and properties go here
+		});
+
+		return Collection;
+	}
+};
